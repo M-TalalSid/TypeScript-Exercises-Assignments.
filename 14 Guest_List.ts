2bc0165031eac2fa67e74e317c5ff5e3d2522c10
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-02-22 15:40:07 +0500

    Add files via upload

---
+++ 14 Guest_List.ts
@@ -0,0 +1,4 @@
+let Guest_list : string[] = ['ShahRukh Khan','Robert Downey Jr','Hugh Jackman'] ;
+for(let i=0; i<Guest_list.length; i++){
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\n It Is Our Pleasure To Invite You In Our Dinner Party.\n\n Thank You! ')
+}
