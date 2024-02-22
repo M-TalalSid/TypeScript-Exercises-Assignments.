ac5ed21b5d8fd2ca2bff46ffb0263de32790a467
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-02-22 15:39:26 +0500

    Add files via upload

---
+++ 13 Your_Own_Array.ts
@@ -0,0 +1,4 @@
+let Transportation : string[] = ['Rolls Royce','Lamborghini','SuperBike','Private Jet' ] ;
+for(let i=0; i<Transportation.length; i++){
+    console.log('I Would Like To Own ' + Transportation[i])
+}
