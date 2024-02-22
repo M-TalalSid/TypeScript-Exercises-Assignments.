f62fede965b1cad677a692bc00a1a5effa76b8ff
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-02-22 15:39:00 +0500

    Add files via upload

---
+++ 12 Greetings.ts
@@ -0,0 +1,5 @@
+let members: string[] = ['Mubashir', 'Hazique', 'Anas', 'Afridi', 'Ahsan', 'Faraz'];
+let message = 'All Are Ready To Go On Pak Tour? ';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
