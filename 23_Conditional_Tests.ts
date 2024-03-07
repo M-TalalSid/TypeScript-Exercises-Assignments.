fd4436597b39224aaadabac95a10384830d7aabc
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-03-07 15:45:10 +0500

    Add files via upload

---
+++ 23_Conditional_Tests.ts
@@ -0,0 +1,43 @@
+// Yahan Mai Ne Car Ka Variable Banaya Hai
+
+let car : String = 'Honda';
+
+// Test No 1
+console.log("Is car === 'Honda'? I Predict True.");
+console.log(car === 'Honda');
+
+// Test No 2
+console.log("Is car !== 'Suzuki'? I Predict True.");
+console.log(car !== 'Suzuki');
+
+// Test No 3
+console.log("Is car == 'Honda'? I Predict True.");
+console.log(car == 'Honda');
+
+// Test No 4
+console.log("Is car !== 'Hyundai'? I Predict True. ");
+console.log(car !== 'Hyundai');
+
+// Test No 5
+console.log("Is car !== 'Kia'? I Predict True");
+console.log(car !== 'Kia');
+
+// Test No 6
+console.log("Is car == 'Bike'? I Predict False");
+console.log(car == 'Bike');
+
+// Test No 7
+console.log("Is car == 'Ferrari'? I Predict False");
+console.log(car == 'Ferrari');
+
+// Test No 8
+console.log("Is car == 'Mustang'? I Predict False");
+console.log(car == 'Mustang');
+
+// Test No 9
+console.log("Is car == 'Lamborghini'? I Predict False");
+console.log(car == 'Lamborghini');
+
+// Test No 10
+console.log("Is car.toUpperCase() == 'Honda'? I Predict False");
+console.log(car.toUpperCase() == 'Honda');
