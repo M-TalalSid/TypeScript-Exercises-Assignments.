4aa2213806a9e39891090bf99823ec01c016594d
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-03-07 15:44:54 +0500

    Add files via upload

---
+++ 22_Intentional_Error.ts
@@ -0,0 +1,13 @@
+// If You Haven’t Received An Array Index Error In One Of Your Programs Yet, Try To Make One Happen.
+
+// Yahan Mai Ne Ek New Array Banayi Hai
+
+let newArray : string[] = ['Robert Downey Jr', 'James Gordan', 'Christian Bale'];
+
+//  Yahan Mai Ne JaanBooj K Error Produce Krnay Liye 5 Likha Hai
+
+console.log(newArray[5]);
+
+// Yahan Ab Program Close Krnay Se Pehlay Error Theek Kiya Hai
+
+console.log(newArray);
