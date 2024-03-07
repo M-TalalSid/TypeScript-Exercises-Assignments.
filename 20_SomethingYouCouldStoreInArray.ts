5c032143dcf435f043d92755135de824719fbb1a
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-03-07 15:40:45 +0500

    Add files via upload

---
+++ 20_SomethingYouCouldStoreInArray.ts
@@ -0,0 +1,16 @@
+// Think Of Something You Could Store In A Array
+
+let items : string[] = ['FB Area', 'Karachi', 'Pakistan'];
+
+// Wrtie A Program That Creates Listing These Items
+
+let item2 : string[] = [];
+item2.push('Germamy');
+
+item2.push('United States');
+
+item2.push('Italy');
+
+console.log(items);
+
+console.log(item2);
