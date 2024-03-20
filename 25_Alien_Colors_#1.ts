dd8f2593ac0736b482258a20c52d487737d11354
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-03-21 03:48:21 +0500

    Add files via upload

---
+++ 25_Alien_Colors_#1.ts
@@ -0,0 +1,20 @@
+// Imagine An Alien Was Just Shot Down In A Game. Create A Variable Called Alien_Color And Assign It A Value Of 'Green', 'Yellow', Or 'Red'.
+
+// Creating A Variable Alien_Color
+
+let alien_color : string =  ' Green ';
+
+// Write An If Statement To Test Whether The Alien’s Color Is Green. If It Is, Print A Message That The Player Just Earned 5 Points.
+
+if(alien_color === ' Green '){
+    console.log(" Player Just Earned 5 Points !! ");
+}
+
+// Write One Version Of This Program That Passes The IF Test And Another That Fails. (The Version That Fails Will Have No Output.)
+
+alien_color = ' Red ';
+
+if(alien_color === ' Green '){
+    console.log(" Player Just Earned 5 Points !! ");
+}
+
