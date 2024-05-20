565689e5a8b5ce3fec3ffef3e050f4d6b96c5bcf
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2024-05-21 03:08:24 +0500

    Add files via upload

---
+++ 36.T-Shirts.ts
@@ -0,0 +1,17 @@
+// T-Shirt: Write A Function Called make_shirt() That Accepts A Size And The Text Of A Message That Should Be Printed On The Shirt.
+
+// The Function Should Print A Sentence Summarizing The Size Of The Shirt And The Message Printed On It.
+
+// Call The Function.
+
+function make_shirt(size : string, text : string){
+
+    console.log(`Creating A ${size} Shirt With The Message : ${text}`);
+
+}
+
+make_shirt("Small", "Big Dawg Things");
+
+make_shirt("Medium", "Big Dawg Things");
+
+make_shirt("Large", "Big Dawg Things");
